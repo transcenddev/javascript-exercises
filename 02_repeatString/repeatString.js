b2192dd74f176b569aa765c758938bf21fe83652
transcenddev <97653516+transcenddev@users.noreply.github.com>
2025-02-15 13:46:51 +0800

    Add repeatString 4

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,29 @@
-const repeatString = function() {
+// make the words repeat next to each other without spaces
+function repeatString(string, times) {
+  let repeatString = "";
 
+  while (times > 0) { // As long as times is greater than 0, the statement is executed
+    repeatString += string; // Same as repeatString = repeatString + string;
+    times--; // Same as times = times - 1;
+  }
+  /* While loop logic
+                      Condition       T/F       repeatString += string      repeatString        times
+    First iteration    (3 > 0)        true            "" + "abc"                  "abc"               2
+    Second iteration   (2 > 0)        true           "abc" + "abc"               "abcabc"             1
+    Third iteration    (1 > 0)        true          "abcabc" + "abc"            "abcabcabc"           0
+    Fourth iteration   (0 > 0)        false
+    }
+  */
+  
+  // Step 3. Return the repeated string
+  return repeatString; // "abcabcabc"
 };
+
+repeatString("hey", 3);
+repeatString("hello", 11);
+repeatString("hi", 1);
+repeatString("bye", 0);
+
 
 // Do not edit below this line
 module.exports = repeatString;
--- 02_repeatString/repeatString.spec.js
+++ 02_repeatString/repeatString.spec.js
@@ -4,13 +4,13 @@ describe('repeatString', () => {
   test('repeats the string', () => {
     expect(repeatString('hey', 3)).toEqual('heyheyhey');
   });
-  test.skip('repeats the string many times', () => {
+  test('repeats the string many times', () => {
     expect(repeatString('hello', 10)).toEqual('hellohellohellohellohellohellohellohellohellohello');
   });
-  test.skip('repeats the string 1 times', () => {
+  test('repeats the string 1 times', () => {
     expect(repeatString('hi', 1)).toEqual('hi');
   });
-  test.skip('repeats the string 0 times', () => {
+  test('repeats the string 0 times', () => {
     expect(repeatString('bye', 0)).toEqual('');
   });
   test.skip('does not use the built-in String repeat method', () => {
